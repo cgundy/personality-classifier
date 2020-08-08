bc0e3233d9b5c180ad377f27a071027f35a7c9a7
cgundy <cgundy@alumni.nd.edu>
2020-08-08 15:42:22 -0500

    everything works

--- api/ml/model.py
+++ api/ml/model.py
@@ -1,103 +1,41 @@
-import numpy as np
-import pandas as pd
-import pickle
-import mypy
-import logging
 from typing import List, Set, Dict, Tuple, Optional, Union, Any
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction import text
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
-import utilities as utils
+from sklearn.base import BaseEstimator
 
 
-class TfidfFit:
-	def __init__(self, tfidf_params: List[Any], my_stop_words: List[str]=None):
-		self._tfidf_params = tfidf_params
-		self._my_stop_words = my_stop_words
-		self._tfidf: TfidfVectorizer
-		self._filepath = utils.file_handler('models','tfidf')
+class MyClassifier(BaseEstimator):
 
-	#Todo: how to ensure that this is not run if tdifdf exists
-	def train_tfidf(self):
-		my_stop_words = self._my_stop_words
-		if not my_stop_words:
-			my_stop_words = []
-		tfidf = TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS.union(my_stop_words), 
-								**self._tfidf_params)
-		return tfidf
-
-	#Todo: ensure that _tfidf has been loaded
-	def fit_tfidf(self, text_input: List[str]) -> np.array:
-		features = self._tfidf.fit_transform(text_input).toarray()
-		return features
-
-	def refit_tfidf(self, text_input: List[str]) -> np.array:
-		tf1_new = TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS.union(self._my_stop_words), 
-									**self._tfidf_params, vocabulary=self._tfidf.vocabulary_)
-		features = tf1_new.fit_transform(text_input).toarray()
-		return features
-
-	def save(self):
-		if self._tfidf is not None:
-			with open(self._filepath, 'wb') as infile:
-				pickle.dump(self._tfidf, infile)
+	def __init__(
+		self, 
+		config: Dict,
+		classifier_type: str
+	):
+		"""
+		A Custom BaseEstimator that can switch between classifiers.
+		:param estimator: sklearn object - The classifier
+		""" 
+		self.config = config
+		if classifier_type == 'LogisticRegression':
+			self.estimator = LogisticRegression(**config['model_parameters'][classifier_type])
+		elif classifier_type == 'RandomForest':
+			self.estimator = RandomForestClassifier(**config['model_parameters'][classifier_type])
+		elif classifier_type == 'SVC':
+			self.estimator = SVC(**config['model_parameters'][classifier_type])
 		else:
-			raise TypeError("The tfidf vector is not trained yet, use .fit_tfidf() before saving")
+			raise TypeError("Invalid model type")
 
-	def load(self):
-		try:
- 		  	self._tfidf = np.load(self._filepath, allow_pickle=True)
-		except:
-			raise TypeError(
-			"The tfidf vector is not trained yet, use .fit_tfidf() before loading")
+	def fit(self, X: List[Any], y: Optional[List[str]]=None, **kwargs) -> BaseEstimator:
+		self.estimator.fit(X, y)
+		return self
 
+	def predict(self, X: List[str], y: Optional[List[str]]=None) -> str:
+		return self.estimator.predict(X)
 
-class Model:
-	def __init__(self, model_type:str, config):
-		self.model_type: str  = model_type
-		self._model_config = config
-		self._model: Union[LogisticRegression, RandomForestClassifier,
-		SVC, MultinomialNB]
-		self._accuracy: float
-		self._model_path = utils.file_handler('models', model_type)
-		self._accuracy_path = utils.file_handler('accuracy',model_type)
+	def predict_proba(self, X: List[str]) -> float:
+		return self.estimator.predict_proba(X)
 
-	def train(self, X: np.array, y: str) -> None:
-		model = None
-		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 28)
-		if self.model_type == 'LogisticRegression':
-	   		model = LogisticRegression(**self._model_config['lr_params']).fit(X_train, y_train)
-		elif self.model_type == 'RandomForest':
-			model = RandomForestClassifier(**self._model_config['rf_params']).fit(X_train, y_train)
-		elif self.model_type == 'SVC':
-			model = SVC(**self._model_config['svc_params']).fit(X_train, y_train)
-		self._model = model	
-		self._accuracy = self._model.score(X_test,y_test)
-
-	def save(self):
-		if self._model is not None and self._accuracy is not None:
-			with open(self._model_path, 'wb') as infile:
-				pickle.dump(self._model, infile)
-			with open(self._accuracy_path, 'wb') as infile:
-				pickle.dump(self._accuracy, infile)
-		else:
-			raise TypeError("The model is not trained yet, use .train() before saving")
-
-	def load(self, request):
-		try:
-			if request=='model':
-				self._model = np.load(self._model_path, allow_pickle=True)
-			elif request == 'accuracy':
-				self._accuracy = np.load(self._accuracy_path, allow_pickle=True)
-		except:
-			raise TypeError(f"The model is not trained yet, use .train() before loading. {self._model_path}")
-
-	def predict(self, features: np.array) -> str:
-		prediction = self._model.predict(features)
-		return prediction[0]
-
-
+	def score(self, X: List[str], y: List[str]) -> float:
+		return self.estimator.score(X, y)
--- api/ml/model_pipeline.py
+++ api/ml/model_pipeline.py
@@ -1,84 +1,63 @@
 import logging
-from model import Model
+import pandas as pd
+from typing import List, Set, Dict, Tuple, Optional, Union, Any
+
+from .model import MyClassifier
+from .utilities import get_config, file_handler, save, load
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import text
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
 
 
+config = get_config()
 logger = logging.getLogger('model')
-#self._model_path: str = model_dir / Path(model_type + '.pkl')
-#self._tfidf_path: str = model_dir / 'tfidf_vec.pkl'
 
 
-class Pipeline:
-	def __init__(self, config, model_type):
-		self.config = utils.get_config()
-		self.model_type = model_type
-		self.tfidf
-
-	def get_tfidf(self, train=False):
-		tfidf = TfidfFit(self.config['tfidf_params'], self.config['my_stop_words'])
-		if train:
-			logger.info("""Training tfidf.""")
-			tfidf.train_tfidf()
-			tifdf.save()
-		else:
-			logger.info("""Loading tfidf.""")
-			tfidf.load()
-		self.tfidf = tfidf
-
-	@staticmethod
-	def _remove_types(text: str) -> str:
-		for ptype in config['types']:
-			if ptype in text or ptype.upper() in text:
-				text = text.replace(ptype, "")
-				text = text.replace(ptype+'s', "")
-				text = text.replace(ptype+'\'s', "")
-				text = text.replace(ptype.upper(), "")
-				text = text.replace(ptype.upper()+'s', "")
-				text = text.replace(ptype.upper()+'\'s', "")
-		return text
-
-	def prep_data(df:pd.DataFrame) -> Tuple[pd.Series, pd.Series]:
-
-		data_file = utils.file_handler()
-		raw_data = pd.read_csv(data_file)
-
-		logger.info("""Prepping data.""")
-		df.columns = ['label', 'text']
-		for index, row in df.iterrows():
-			df.text[index] = _remove_types(df.text[index])
-		self.label = df['label']
-		self.text = df['text']
-
-	def prep_features(self, train=False):
-		if train:
-			self.features = self.tfidf.fit_tfidf(self.text)
-		else:
-			self.features = self.tfidf.refit_tfidf(self.text)
-
-	def train_model(self) -> None:
-		model = Model(self.model_type, self.config)
-		logger.info("Training {model_type} model.")
-		model.train(self.features, self.labels)
-		logger.info("Saving {model_type} model.")
-		model.save()
-
-	def score(self) -> str:
-		model = Model(self.model_type)
-		model.load('model')
-		y_pred = model.predict(self.features)
-
-	def get_accuracy(self) -> float:
-		model = Model(self.model_type)
-		model.load('accuracy')
-		self.accuracy = model._accuracy
+def _remove_types(text: str) -> str:
+	for ptype in config['types']:
+		if ptype in text or ptype.upper() in text:
+			text = text.replace(ptype, "")
+			text = text.replace(ptype+'s', "")
+			text = text.replace(ptype+'\'s', "")
+			text = text.replace(ptype.upper(), "")
+			text = text.replace(ptype.upper()+'s', "")
+			text = text.replace(ptype.upper()+'\'s', "")
+	return text
 
 
-def train_pipeline(Pipeline, retrain_tfidf=False):
-	Pipeline.get_tfidf(retrain_tfidf)
-	Pipeline.prep_data()
-	Pipeline.prep_features()
-	Pipeline.train_model()
+def train(model_type: str) -> None:
+	data_file = file_handler('data')
+	df = pd.read_csv(data_file)
+	df.columns = ['label', 'text']
+	for index, row in df.iterrows():
+		df.text[index] = _remove_types(df.text[index])
+	X_train, X_test, y_train, y_test = train_test_split(df.text, df.label, test_size=0.2, random_state = 28)
 
-def score_pipeline(Pipeline):
-	Pipeline.get_tfidf()
-	Pipeline.prep_features()
-	Pipeline.score()
+	pipe = Pipeline([
+		('tfidf', TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS.union(config['my_stop_words']), 
+									**config['model_parameters']['tfidf'])),
+		('clf', MyClassifier(config, model_type))])
+
+	pipe.fit(X_train, y_train)
+	save(pipe, 'pipeline', model_type)
+	save(pipe.score(X_test, y_test), 'accuracy', model_type)
+
+def predict(text_input: List[str], model_type: str) -> str:
+	pipe = load('pipeline', model_type)
+
+	#pipe = Pipeline([
+	#	('tfidf', TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS.union(config['my_stop_words']), 
+	#									**config['model_parameters']['tfidf'], vocabulary=tfidf.vocabulary_))])
+
+#	pipe.fit_transform([text_input])
+#	pipe.steps.append(('clf', model))
+	return pipe.predict(text_input)
+
+def get_accuracy(model_type: str) -> float:
+	return load('accuracy', model_type)
+
+#train('RandomForest')
+#print(predict(['hello my name is Carly'],'RandomForest'))
+#print(get_accuracy('RandomForest'))
